27d7d7fd0af5c5d9ad5a297d64e0937506f9f8b1
Daniele Santini <danysan95@gmail.com>
2017-02-26 11:17:24 +0100

    Implementa inversa

--- ViennaCLTest.cpp
+++ ViennaCLTest.cpp
@@ -4,10 +4,14 @@
 #include <vector> // std::vector
 using namespace std;
 
+// http://viennacl.sourceforge.net/doc/manual-operations.html
+// http://viennacl.sourceforge.net/doc/manual-algorithms.html#manual-algorithms-eigenvalues
 #include <viennacl/linalg/norm_2.hpp>
 #include <viennacl/linalg/inner_prod.hpp>
 #include <viennacl/linalg/prod.hpp>
 #include <viennacl/linalg/direct_solve.hpp>
+#include <viennacl/linalg/eig.hpp>
+#include <viennacl/linalg/lu.hpp>
 using namespace viennacl::linalg;
 
 viennacl::vector<float> leggiVettore(unsigned dimensione){
@@ -28,6 +32,8 @@ viennacl::vector<float> leggiVettore(unsigned dimensione){
     // Copia i valori (per la GPU)
     viennacl::vector<float> cl_vec(dimensione);
     viennacl::copy(vec, cl_vec);
+
+    cout << "Vettore: " << cl_vec << endl;
     return cl_vec;
 }
 
@@ -56,6 +62,30 @@ viennacl::matrix<float> leggiMatrice(unsigned righe, unsigned colonne){
     // Copia i valori (per la GPU)
     viennacl::matrix<float> cl_mat(righe, colonne);
     viennacl::copy(mat, cl_mat);
+
+    cout << "Matrice: " << cl_mat << endl;
+    return cl_mat;
+}
+
+viennacl::matrix<float> identita(unsigned ordine){
+    // Prepara la dimensione
+    while(ordine == 0){
+        cout << "Ordine matrice? ";
+        cin >> ordine;
+    }
+
+    // Prepara la matrice (per la CPU)
+    vector<vector<float> > mat(ordine);
+    for(unsigned i=0; i<ordine; i++){
+        mat[i] = vector<float>(ordine);
+        for(unsigned j=0; j<ordine; j++){
+            mat[i][j] = (i==j);
+        }
+    }
+
+    // Copia i valori (per la GPU)
+    viennacl::matrix<float> cl_mat(ordine, ordine);
+    viennacl::copy(mat, cl_mat);
     return cl_mat;
 }
 
@@ -65,7 +95,7 @@ void modulo(){
     viennacl::scalar<float> ris = norm_2(vec);
     viennacl::backend::finish();
 
-    cout << "Modulo: " << ris << endl << endl;
+    cout << "Modulo: " << ris << endl;
 }
 
 void prodottoScalare(){
@@ -75,7 +105,7 @@ void prodottoScalare(){
     viennacl::scalar<float> ris = inner_prod(vec1, vec2);
     viennacl::backend::finish();
 
-    cout << "Prodotto scalare: " << ris << endl << endl;
+    cout << "Prodotto scalare: " << ris << endl;
 }
 
 void prodottoMatriceVettore(){
@@ -85,7 +115,7 @@ void prodottoMatriceVettore(){
     viennacl::vector<float> ris = prod(mat, vec);
     viennacl::backend::finish();
 
-    cout << "Prodotto matrice*vettore: " << ris << endl << endl;
+    cout << "Prodotto matrice*vettore: " << ris << endl;
 }
 
 void prodottoMatriceMatrice(){
@@ -95,7 +125,7 @@ void prodottoMatriceMatrice(){
     viennacl::matrix<float> ris = prod(mat1, mat2);
     viennacl::backend::finish();
 
-    cout << "Prodotto matriciale: " << ris << endl << endl;
+    cout << "Prodotto matriciale: " << ris << endl;
 }
 
 void sistemaLineare(){
@@ -105,11 +135,23 @@ void sistemaLineare(){
     viennacl::vector<float> ris = solve(mat, vec, upper_tag());
     viennacl::backend::finish();
 
-    cout << "Soluzione: " << ris << endl << endl;
+    cout << "Soluzione: " << ris << endl;
 }
 
+// https://sourceforge.net/p/viennacl/mailman/message/35357381/
 void inversa(){
+    float n;
+    cout << "Ordine della matrice? ";
+    cin >> n;
 
+    viennacl::matrix<float> mat1 = leggiMatrice(n,n),
+            mat2 = identita(n);
+
+    lu_factorize(mat1);
+    lu_substitute(mat1, mat2);
+    viennacl::backend::finish();
+
+    cout << "Inversa: " << mat2 << endl;
 }
 
 void autovettori(){
