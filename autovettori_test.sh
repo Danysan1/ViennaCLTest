2d9963dac570a863a4a989abfa36b25531e6bcf4
Daniele Santini <danysan95@gmail.com>
2017-02-26 17:32:32 +0100

    Migliora autovettori_test

--- autovettori_test.sh
+++ autovettori_test.sh
@@ -3,6 +3,4 @@
 echo '    [ 2 1 0 ]                  [ 4 ]                  [ 1 -1  1 ]'
 echo 'A = [ 1 3 1 ] ==> autovalori = [ 2 ]    autovettori = [ 2  0 -1 ]'
 echo '    [ 0 1 2 ]                  [ 1 ]                  [ 1  1  1 ]'
-
-echo -e $toOut
 printf 'a\n3\n2\n1\n0\n1\n3\n1\n0\n1\n2' | ./ViennaCLTest
